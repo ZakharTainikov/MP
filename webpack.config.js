0f3deda583307a2d4f882467da272bf0564a92cf
zaktai2 <34128710+zaktai2@users.noreply.github.com>
2017-12-14 14:53:56 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry:
+    {
+        task1: "./source/Task1.js",
+        polys: [
+            "babel-polyfill",
+            "promise-polyfill",
+            "fetch-polyfill"
+        ]
+    }
+    ,
+    output: {
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].chunk.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["env", "es2015-ie"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
