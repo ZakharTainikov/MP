45a8ab33bb1568773bbcea7b59e7b65a6867116d
zaktai2 <34128710+zaktai2@users.noreply.github.com>
2018-01-24 17:30:14 +0300

    Update newsMod.js

--- source/newsMod.js
+++ source/newsMod.js
@@ -1,4 +1,6 @@
-﻿import "./styles.css";
+import "./styles.css";
+
+import * as pat from './patterns';
 
 
 export function getApiKey() {
@@ -10,23 +12,7 @@ export default function loadNewsSourses() {
         .then(r => r.json())
         .then(data => {
             let { sources } = data;
-            const markup = `                
-                ${sources.map(source =>
-                    `<div class="newsSource">
-                        <table width="100%">
-                            <tr>
-                                <td align="center">
-                                    <h3>${source.name}</h3>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td align="center">                                    
-                                    <span class="sourceLink" data-id='${source.id}'>${source.url}</span>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>`
-                ).join('')}`;
+            const markup = `${sources.map(sourceInfo => pat.newsFactory().createNewsSource(sourceInfo).toHtml()).join('')}`;
             document.getElementById("newsContainer").innerHTML = markup;
             attachDisplayNewsHandler();
         })
@@ -56,25 +42,7 @@ function displayNews(sourceId) {
             let { articles } = data;
             const markup = `
                 <table width="100%">
-                ${articles.map(article =>
-                    `<tr>
-                        <td align="center">
-                            <div>
-                                <h3 class="colorRed">${article.title}</h3>
-                            </div>
-                            <div class="img" style="background-image: url('${article.urlToImage ? article.urlToImage : ''}')">
-                                <img class="img" src='${article.urlToImage ? article.urlToImage : ''}' style="visibility: hidden;" />
-                            </div>                            
-                            <div>
-                                <span>${article.description}</span>
-                            </div>
-                            <div class="newsAuthor">
-                                <b>${article.author ? article.author : ''}</b><br>
-                                <span>${article.publishedAt ? new Date(article.publishedAt).toLocaleDateString() : ''}</span>
-                            </div>
-                        </td>
-                    </tr>`
-                ).join('')}
+                    ${articles.map(articleInfo => pat.newsFactory().createArticle(articleInfo).toHtml()).join('')}
                 </table>`;
             document.getElementById("newsScreen").innerHTML = markup;
             scroll(0, 0);
